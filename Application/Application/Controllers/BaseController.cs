97e5259ed0ab6e2627eb019ab2cae5394abb1f6b
ereceb8 <94682942+ereceb8@users.noreply.github.com>
2022-01-27 10:23:09 +0200

    initial commit

---
+++ Application/Application/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    public abstract class BaseController:Controller
+    {   
+    
+    
+    
+    
+    
+    }
+
+
+
+
+}
---
+++ Application/Common/Global.cs
@@ -0,0 +1,9 @@
+﻿namespace Common
+{
+    public static class Global
+    {
+        public static string ApplicationName { get; } = "MyApp";
+
+        public static string AdminRoleName { get; } = "Admin";
+    }
+}
